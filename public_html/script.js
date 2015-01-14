d87e81ac270bc30d8c0df3692e6566a8448bd080
Joshua Gassama <JoshuaGassama@yahoo.com>
2015-01-13 21:55:56 -0800

    video 7

--- public_html/script.js
+++ public_html/script.js
@@ -57,5 +57,7 @@ $("document").ready(function(){
     
     function mouseClick(){
           
-    $ ('p').html ('taaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaayah');
+    $ ('p').html ('swag');
     }
+    
+    
